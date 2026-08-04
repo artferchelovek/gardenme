bd227334ce9b02071c13b1b3f31702343450dae8
Artem Shutov <artfergames@gmail.com>
2026-08-05 00:50:59 +0700

    feat: мокап уведомлений

--- src/pages/PlantView/PlantView.tsx
+++ src/pages/PlantView/PlantView.tsx
@@ -4,6 +4,7 @@ import { useNavigate, useParams } from "react-router-dom";
 import { type Plant, plantApi } from "@/api/plantApi.ts";
 import DeviceStatus from "@/components/DeviceStatus/DeviceStatus.tsx";
 import MoistureChart from "@/components/MoistureChart/MoistureChart.tsx";
+import PlantNotification from "@/components/PlantNotification/PlantNotification.tsx";
 import { PLANT_STATUS } from "@/types/plant.ts";
 import { type ChartPoint, get24hMoisture } from "@/utils/chartPoints.ts";
 
@@ -84,6 +85,7 @@ export default function PlantView() {
           <CurrentMoisture plant={plant} statusText={statusText} />
           <MoistureChart chart={chartData} />
           <DeviceStatus plant={plant} />
+          <PlantNotification />
         </>
       )}
     </div>
